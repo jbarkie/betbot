6310f5580b1848b5da4e959893d81be8abe92551
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-20 20:48:09 -0600

    Add component.

--- frontend/src/app/components/nba/nba.component.ts
+++ frontend/src/app/components/nba/nba.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { GamesListComponent } from '../basketball/games-list/games-list.component';
 
 @Component({
-  selector: 'app-nba',
-  standalone: true,
-  imports: [DatePipe],
-  templateUrl: './nba.component.html',
-  styleUrl: './nba.component.css'
+    selector: 'app-nba',
+    standalone: true,
+    templateUrl: './nba.component.html',
+    styleUrl: './nba.component.css',
+    imports: [DatePipe, GamesListComponent]
 })
 export class NbaComponent {
   today: Date = new Date();

8fa146db9d740c40d394659cbe6949243af1b00f
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-20 18:52:56 -0600

    Add route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BasketballComponent } from './components/basketball/basketball.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basketball',
+    component: BasketballComponent,
+  },
+];

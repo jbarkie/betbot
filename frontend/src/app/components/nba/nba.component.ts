1503265b0f279b028a66cae8a8378bbf715e861f
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-23 11:24:16 -0600

    Refactor component

--- frontend/src/app/components/nba/nba.component.ts
+++ frontend/src/app/components/nba/nba.component.ts
@@ -1,14 +1,24 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { GamesListComponent } from '../basketball/games-list/games-list.component';
+import { GamesListComponent } from '../games-list/games-list.component';
+import { Game } from '../models';
 
 @Component({
-    selector: 'app-nba',
-    standalone: true,
-    templateUrl: './nba.component.html',
-    styleUrl: './nba.component.css',
-    imports: [DatePipe, GamesListComponent]
+  selector: 'app-nba',
+  standalone: true,
+  templateUrl: './nba.component.html',
+  styleUrl: './nba.component.css',
+  imports: [DatePipe, GamesListComponent],
 })
 export class NbaComponent {
   today: Date = new Date();
+  game1: Game = {
+    id: '1',
+    sport: 'NBA',
+    awayTeam: 'LAL',
+    homeTeam: 'BOS',
+    date: '2024-03-25',
+    odds: 'BOS -9.5',
+  };
+  games = [this.game1];
 }

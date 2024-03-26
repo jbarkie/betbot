cae5f645c9a81d2ad9a7ebbaf3d4b20b2bb9a0e5
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-26 13:46:51 -0600

    Update config
    Add providers.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { reducers } from './state';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { nbaFeature } from './components/nba/state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore(reducers), provideHttpClient(), provideState(nbaFeature), provideStoreDevtools()],
 };

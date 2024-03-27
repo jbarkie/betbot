63e01097c873767f40479c54f9d4c8c0337cb597
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-26 22:41:33 -0600

    Add loading animation

--- frontend/src/app/components/games-list/games-list.component.ts
+++ frontend/src/app/components/games-list/games-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Game } from '../models';
-import { AlertMessageComponent } from "../alert-message/alert-message.component";
-import { GameComponent } from "../game/game.component";
-
+import { AlertMessageComponent } from '../alert-message/alert-message.component';
+import { GameComponent } from '../game/game.component';
+import { Store } from '@ngrx/store';
+import { nbaFeature } from '../nba/state';
+import { NbaCommands } from '../nba/state/actions';
 
 @Component({
-    selector: 'app-games-list',
-    standalone: true,
-    templateUrl: './games-list.component.html',
-    styleUrl: './games-list.component.css',
-    imports: [AlertMessageComponent, GameComponent]
+  selector: 'app-games-list',
+  standalone: true,
+  templateUrl: './games-list.component.html',
+  styleUrl: './games-list.component.css',
+  imports: [AlertMessageComponent, GameComponent],
 })
 export class GamesListComponent {
+  private store = inject(Store);
   list = input.required<Game[]>();
-  
+  loaded = this.store.selectSignal(nbaFeature.selectIsLoaded);
 }

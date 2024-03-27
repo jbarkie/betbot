4c533d25113cbaeb91c25f52608afdc7d27860e8
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-26 22:45:11 -0600

    Add actions
    For loading NBA games.

---
+++ frontend/src/app/components/nba/state/actions.ts
@@ -0,0 +1,16 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Game } from '../../models';
+
+export const NbaCommands = createActionGroup({
+  source: 'NBA Commands',
+  events: {
+    'Load NBA Games': emptyProps(),
+  },
+});
+
+export const NBADocuments = createActionGroup({
+  source: 'NBA Documents',
+  events: {
+    Games: props<{ payload: Game[] }>(),
+  },
+});

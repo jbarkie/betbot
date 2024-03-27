3a934611f92bf1709f7b6db07865b372061b4316
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-26 22:48:46 -0600

    Update config
    Enable CORS.

--- api/main.py
+++ api/main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
+from nba import get_todays_games
+from models.game import Game
 import uvicorn
+from typing import Any
+from fastapi.middleware.cors import CORSMiddleware
 
 def main():
     app = FastAPI()
+    configure(app)
 
     @app.get("/status")
     async def status():
         return {"status": "ok"}
+    
+    @app.get("/nba/games", response_model=list[Game])
+    async def games() -> Any :
+        return get_todays_games()
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+def configure(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost", "http://localhost:4200"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 if __name__ == "__main__":
     main()

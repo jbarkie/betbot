858b05ef1d73db2edcccf0212f79a5f195fca9ef
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-26 22:42:24 -0600

    Update config
    Provide NbaEffects.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,19 @@ import { provideRouter } from '@angular/router';
 import { provideStore, provideState } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { reducers } from './state';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { nbaFeature } from './components/nba/state';
+import { provideEffects } from '@ngrx/effects';
+import { NbaEffects } from './components/nba/state/nba-effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(reducers), provideHttpClient(), provideState(nbaFeature), provideStoreDevtools()],
+  providers: [
+    provideRouter(routes),
+    provideStore(reducers),
+    provideHttpClient(),
+    provideState(nbaFeature),
+    provideEffects([NbaEffects]),
+    provideStoreDevtools(),
+  ],
 };

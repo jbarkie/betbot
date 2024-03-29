b83daf16580832b45ec071eb541535512197f924
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-28 18:21:59 -0600

    Refactor images
    Make markup containing team logos more readable and maintainable.

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -9,26 +9,32 @@ import { Game } from '../models';
   template: ` 
   <div class="card card-side bg-base-100 shadow-xl mx-auto my-5 max-w-2xl">
   <figure>
-    <img [src]="'assets/img/' + (game().awayTeam.split(' ').pop()?.toLowerCase()) + '.png'" alt="Away Team Logo" class="w-28 mx-5" />
+    <img [src]="getImageSrc(game().homeTeam)" [alt]="game().homeTeam + ' logo'" class="w-28 mx-5" />
   </figure>
   <div class="card-body">
     <h2 class="card-title">{{ game().awayTeam }} &#64; <br/> {{ game().homeTeam }}</h2>
-    @if(game().odds === "none") {
+    @if(game().odds === {}) {
       <p class="text-md">No odds available</p>
     } @else {
-      <p>{{ game().homeTeam }} ML: {{ game().odds.split(' ')[0] }}</p>
-      <p>{{ game().awayTeam }} ML: {{ game().odds.split(' ')[1] }}</p>
+      <p>{{ game().homeTeam }} ML: {{ game().odds['home'] }}</p>
+      <p>{{ game().awayTeam }} ML: {{ game().odds['away'] }}</p>
     }
     <div class="card-actions justify-end">
       <button class="btn btn-primary">Analyze</button>
     </div>
   </div>
   <figure>
-    <img [src]="'assets/img/' + (game().homeTeam.split(' ').pop()?.toLowerCase()) + '.png'" alt="Home Team Logo" class="w-28 mx-5" />
+    <img [src]="getImageSrc(game().awayTeam)" [alt]="game().awayTeam + ' logo'" class="w-28 mx-5" />
   </figure>
 </div>`,
   styles: [],
 })
 export class GameComponent {
   game = input.required<Game>();
+
+  getImageSrc(teamString: string) {
+    const words = teamString.split(' ');
+    const teamName = words[words.length - 1].toLowerCase();
+    return `assets/img/${teamName}.png`;
+  }
 }

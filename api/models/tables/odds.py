01c6864193eb56bd3008754ecfaf62ffdccf63a3
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-29 20:11:22 -0600

    Add initial migration
    To add odds table to database.

---
+++ api/models/tables/odds.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Odds(Base):
+    __tablename__ = 'odds'
+
+    id = Column(String, primary_key=True)
+    time = Column(DateTime)
+    home_odds = Column(String)
+    away_odds = Column(String)
+    home_team = Column(String)
+    away_team = Column(String)

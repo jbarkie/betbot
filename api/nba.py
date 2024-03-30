91c62d0deec350cdaa6308589476b69790da1d39
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-03-29 20:13:45 -0600

    Cache odds in database
    And check before fetching odds from API to limit number of network calls.

--- api/nba.py
+++ api/nba.py
@@ -1,13 +1,18 @@
+from models.tables.odds import Odds
 from nba_api.live.nba.endpoints import scoreboard
 from models.nba import Game, GamesResponse
 from dateutil import parser
-from datetime import timezone
-from config import ODDS_API_URL
+from datetime import timezone, datetime
+from config import ODDS_API_URL, DB_URL
 from utils import format_american_odds
 import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pprint
 
-def fetch_odds(home_team, away_team):
+def fetch_odds(game_id, home_team, away_team):
     response = requests.get(ODDS_API_URL).json()
+    odds = {}
     for game in response:
         if game['home_team'] == home_team and game['away_team'] == away_team:
             fanduel_bookmaker = next((bm for bm in game.get('bookmakers', []) if bm['key'] == 'fanduel'), None)
@@ -15,23 +20,66 @@ def fetch_odds(home_team, away_team):
                 h2h_market = next((market for market in fanduel_bookmaker.get('markets', []) if market['key'] == 'h2h'), None)
                 if h2h_market:
                     outcomes = {outcome['name']: outcome['price'] for outcome in h2h_market.get('outcomes', [])}
-                    return {
-                        'home': format_american_odds(outcomes.get(home_team, 0)),
-                        'away': format_american_odds(outcomes.get(away_team, 0))
-                    }
-    return {}
+                    odds['home'] = format_american_odds(outcomes.get(home_team, 0))
+                    odds['away'] = format_american_odds(outcomes.get(away_team, 0))
+    if not is_odds_data_cached(game_id) and odds:
+        pprint.pp({'game_id': game_id, 'home_team': home_team, 'away_team': away_team, 'odds': odds})
+        cache_odds(game_id, odds, home_team, away_team)
+    return odds
+
+def connect_to_db():
+    engine = create_engine(DB_URL)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def cache_odds(game_id, odds_data, home_team, away_team):
+    session = connect_to_db()
+    game = Odds(
+        id=game_id,
+        time=datetime.now(),
+        home_odds=odds_data['home'],
+        away_odds=odds_data['away'],
+        home_team=home_team,
+        away_team=away_team
+    )
+    session.add(game)
+    session.commit()
+
+def is_odds_data_cached(game_id):
+    session = connect_to_db()
+    odds_data = session.query(Odds).filter_by(id=game_id).first()
+    odds_data_dict = {}
+    if odds_data:
+        odds_data_dict['home'] = odds_data.home_odds
+        odds_data_dict['away'] = odds_data.away_odds
+        return odds_data_dict
+    else:
+        return None
+    
+def team_name(game, team):
+    city = game[f'{team}Team']['teamCity']
+    if city == "LA":
+        city = "Los Angeles"
+    return f"{city} {game[f'{team}Team']['teamName']}"
 
 def create_game_object(game, date):
-    home_team = f"{game['homeTeam']['teamCity']} {game['homeTeam']['teamName']}"
-    away_team = f"{game['awayTeam']['teamCity']} {game['awayTeam']['teamName']}"
+    home_team = team_name(game, 'home')
+    away_team = team_name(game, 'away')
+    game_id = game['gameId']
+    cached_odds = is_odds_data_cached(game_id)
+    odds = cached_odds if cached_odds else fetch_odds(game_id, home_team, away_team)
+    home_odds = str(odds.get('home'))
+    away_odds = str(odds.get('away'))
     return Game(
-        id=game['gameId'],
+        id=game_id,
         sport='NBA',
         homeTeam=home_team,
         awayTeam=away_team,
         date=date,
         time=parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone(tz=None).strftime("%H:%M"),
-        odds=fetch_odds(home_team, away_team)
+        homeOdds=home_odds,
+        awayOdds=away_odds
     )
 
 def get_todays_games():

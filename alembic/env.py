4490c75b3d5a50e13b27bd72dee2badd7904e3b7
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-04-02 20:33:06 -0600

    Update import statement
    To reflect changes to api organization

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from api.models.tables.odds import Base
+from api.src.models.tables.odds import Base
 
 from alembic import context
 

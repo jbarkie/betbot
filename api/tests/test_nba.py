03ad4cf7235395d5970c3fda8920c303e8aac2b5
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-04-03 21:01:00 -0600

    Update tests
    To reflect new changes to api/src/nba.py

--- api/tests/test_nba.py
+++ api/tests/test_nba.py
@@ -1,8 +1,37 @@
 from fastapi.testclient import TestClient
-from api.src.nba import fetch_odds, is_odds_data_cached, cache_odds, team_name, create_game_object
+from datetime import datetime, timedelta
+from api.src.nba import call_odds_api, is_odds_data_cached, cache_odds, team_name, create_game_object
 from api.src.main import app
 
 client = TestClient(app)
+
+odds_data = [
+    {
+        'home_team': 'Test City Test Team',
+        'away_team': 'Los Angeles Test Team',
+        'bookmakers': [
+            {
+                'key': 'fanduel',
+                'markets': [
+                    {
+                        'key': 'h2h',
+                        'outcomes': [
+                            {'name': 'Test City Test Team', 'price': 1.5},
+                            {'name': 'Los Angeles Test Team', 'price': 2.5}
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+game = {
+        'gameId': 'test_game_id',
+        'gameTimeUTC': '2023-06-08T00:00:00Z',
+        'homeTeam': {'teamCity': 'Test City', 'teamName': 'Test Team'},
+        'awayTeam': {'teamCity': 'LA', 'teamName': 'Test Team'}
+}
 
 def test_status():
     response = client.get("/status")
@@ -14,30 +43,11 @@ def test_nba_games():
     assert response.status_code == 200
     assert len(response.json()['list']) > 0
 
-def test_fetch_odds(mocker):
+def test_call_odds_api(mocker):
     mock_response = mocker.patch('requests.get')
-    mock_response.return_value.json.return_value = [
-        {
-            'home_team': 'Test Home Team',
-            'away_team': 'Test Away Team',
-            'bookmakers': [
-                {
-                    'key': 'fanduel',
-                    'markets': [
-                        {
-                            'key': 'h2h',
-                            'outcomes': [
-                                {'name': 'Test Home Team', 'price': 1.5},
-                                {'name': 'Test Away Team', 'price': 2.5}
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-    odds = fetch_odds('test_game_id', 'Test Home Team', 'Test Away Team')
-    assert odds == {'home': '-200', 'away': '+150'}
+    mock_response.return_value.json.return_value = odds_data
+    odds_response = call_odds_api()
+    assert odds_response == mock_response.return_value.json.return_value
 
 def test_is_odds_data_cached(mocker):
     mock_query = mocker.patch('api.src.nba.connect_to_db')
@@ -46,6 +56,7 @@ def test_is_odds_data_cached(mocker):
     mock_odds_data = mocker.MagicMock()
     mock_odds_data.home_odds = '-200'
     mock_odds_data.away_odds = '+150'
+    mock_odds_data.expires = datetime.now() + timedelta(minutes=10)
     mock_query.return_value.query.return_value.filter_by.return_value.first.return_value = mock_odds_data
     assert is_odds_data_cached('test_game_id') == {'home': '-200', 'away': '+150'}
 
@@ -56,23 +67,17 @@ def test_cache_odds(mocker):
     mock_session.return_value.commit.assert_called_once()
 
 def test_team_name():
-    game = {
+    test_game = {
         'homeTeam': {'teamCity': 'Test City', 'teamName': 'Test Team'},
         'awayTeam': {'teamCity': 'LA', 'teamName': 'Test Team'}
     }
-    assert team_name(game, 'home') == 'Test City Test Team'
-    assert team_name(game, 'away') == 'Los Angeles Test Team'
+    assert team_name(test_game, 'home') == 'Test City Test Team'
+    assert team_name(test_game, 'away') == 'Los Angeles Test Team'
 
 def test_create_game_object(mocker):
-    game = {
-        'gameId': 'test_game_id',
-        'gameTimeUTC': '2023-06-08T00:00:00Z',
-        'homeTeam': {'teamCity': 'Test City', 'teamName': 'Test Team'},
-        'awayTeam': {'teamCity': 'LA', 'teamName': 'Test Team'}
-    }
     mocker.patch('api.src.nba.is_odds_data_cached', return_value=None)
-    mocker.patch('api.src.nba.fetch_odds', return_value={'home': '-200', 'away': '+150'})
-    game_object = create_game_object(game, '2023-06-08')
+    mocker.patch('api.src.nba.cache_odds')
+    game_object = create_game_object(game, '2023-06-08', odds_data)
     assert game_object.id == 'test_game_id'
     assert game_object.sport == 'NBA'
     assert game_object.homeTeam == 'Test City Test Team'
@@ -81,3 +86,10 @@ def test_create_game_object(mocker):
     assert game_object.time == '18:00'
     assert game_object.homeOdds == '-200'
     assert game_object.awayOdds == '+150'
+
+def test_odds_equal_to_one(mocker):
+    mocker.patch('api.src.nba.is_odds_data_cached', return_value=None)
+    mocker.patch('api.src.nba.cache_odds')
+    odds_data[0]['bookmakers'][0]['markets'][0]['outcomes'][0]['price'] = 1
+    game_object = create_game_object(game, '2023-06-08', odds_data)
+    assert game_object.homeOdds == 'None'

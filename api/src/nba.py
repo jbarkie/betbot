5d82185f443f606da5b858253170e56673c74b6e
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-04-16 20:48:56 -0600

    Update response
    Given date argument, return the corresponding list of games and store in database accordingly.

--- api/src/nba.py
+++ api/src/nba.py
@@ -1,37 +1,51 @@
 from api.src.models.tables.odds import Odds
-from nba_api.live.nba.endpoints import scoreboard
 from api.src.models.nba import Game, GamesResponse
 from dateutil import parser
-from datetime import timedelta, timezone, datetime
+from datetime import timedelta, datetime
 from api.src.config import ODDS_API_URL, DB_URL
 from api.src.utils import format_american_odds
 import requests
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, cast, Date, func
 from sqlalchemy.orm import sessionmaker
+from pytz import timezone, utc
 
 def call_odds_api():
     response = requests.get(ODDS_API_URL).json()
     return response
 
-def parse_response_for_game(game, odds_response):
-    odds = {}
-    home_team = team_name(game, 'home')
-    away_team = team_name(game, 'away')
+def parse_response_and_store_games(odds_response):
+    games = []
+    eastern = timezone('US/Eastern')
     for game_data in odds_response:
-        if game_data['home_team'] == home_team and game_data['away_team'] == away_team:
-            fanduel_bookmaker = next((bm for bm in game_data.get('bookmakers', []) if bm['key'] == 'fanduel'), None)
-            if fanduel_bookmaker:
-                h2h_market = next((market for market in fanduel_bookmaker.get('markets', []) if market['key'] == 'h2h'), None)
-                if h2h_market:
-                    outcomes = {outcome['name']: outcome['price'] for outcome in h2h_market.get('outcomes', [])}
-                    odds = {
-                        'home': format_american_odds(outcomes.get(home_team, 0)),
-                        'away': format_american_odds(outcomes.get(away_team, 0))
-                    }
-    if odds != {}:
-        if not update_existing_odds_in_db(game['gameId'], odds):
-            store_odds(game['gameId'], odds, home_team, away_team)
-    return odds
+        home_team = game_data['home_team']
+        away_team = game_data['away_team']
+        commence_time_utc = parser.parse(game_data['commence_time']).replace(tzinfo=utc)
+        commence_time_local = commence_time_utc.astimezone(eastern)
+        game_id = game_data['id']
+        
+        fanduel_bookmaker = next((bm for bm in game_data.get('bookmakers', []) if bm['key'] == 'fanduel'), None)
+        if fanduel_bookmaker:
+            h2h_market = next((market for market in fanduel_bookmaker.get('markets', []) if market['key'] == 'h2h'), None)
+            if h2h_market:
+                outcomes = {outcome['name']: outcome['price'] for outcome in h2h_market.get('outcomes', [])}
+                home_odds = format_american_odds(outcomes.get(home_team, 0))
+                away_odds = format_american_odds(outcomes.get(away_team, 0))
+                
+                game = Game(
+                    id=game_id,
+                    sport='NBA',
+                    homeTeam=home_team,
+                    awayTeam=away_team,
+                    time=commence_time_local.strftime("%Y-%m-%d %H:%M"),
+                    homeOdds=home_odds,
+                    awayOdds=away_odds
+                )
+                games.append(game)
+                
+                if not update_existing_odds_in_db(game_id, home_odds, away_odds):
+                    store_odds(game)
+    
+    return games
 
 def connect_to_db():
     engine = create_engine(DB_URL)
@@ -39,71 +53,60 @@ def connect_to_db():
     session = Session()
     return session
 
-def update_existing_odds_in_db(game_id, odds_data):
+def update_existing_odds_in_db(game_id, home_odds, away_odds):
     session = connect_to_db()
     existing = session.query(Odds).filter_by(id=game_id).first()
     if existing:
-        existing.home_odds = odds_data['home']
-        existing.away_odds = odds_data['away']
+        existing.home_odds = home_odds
+        existing.away_odds = away_odds
         existing.expires = datetime.now() + timedelta(minutes=72)
         session.commit()
         return True
     return False
 
-def store_odds(game_id, odds_data, home_team, away_team):
+def store_odds(game: Game):
     session = connect_to_db()
     game = Odds(
-        id=game_id,
-        time=datetime.now(),
-        home_odds=odds_data['home'],
-        away_odds=odds_data['away'],
-        home_team=home_team,
-        away_team=away_team,
-        expires=datetime.now() + timedelta(minutes=72)
+        id=game.id,
+        time=game.time,
+        home_odds=game.homeOdds,
+        away_odds=game.awayOdds,
+        home_team=game.homeTeam,
+        away_team=game.awayTeam,
+        expires=datetime.strptime(game.time, "%Y-%m-%d %H:%M") + timedelta(minutes=72)
     )
     session.add(game)
     session.commit()
 
-def is_odds_data_cached(game_id):
+def is_data_expired():
     session = connect_to_db()
-    odds_data = session.query(Odds).filter_by(id=game_id).first()
-    return {
-            'home': odds_data.home_odds,
-            'away': odds_data.away_odds
-    } if odds_data and (odds_data.expires is not None and odds_data.expires > datetime.now()) else None
+    current_time = datetime.now()
+    nearest_expiration = session.query(func.min(Odds.expires)).scalar()
 
-def team_name(game, team):
-    city = game[f'{team}Team']['teamCity']
-    if city == "LA":
-        city = "Los Angeles"
-    return f"{city} {game[f'{team}Team']['teamName']}"
+    if nearest_expiration is None:
+        return True
+    
+    return nearest_expiration <= current_time
 
-def get_odds(game, odds_response):
-    game_id = game['gameId']
-    cached_odds = is_odds_data_cached(game_id)
-    return cached_odds if cached_odds else parse_response_for_game(game, odds_response)
-
-def create_game_object(game, date, odds_response):
-    home_team = team_name(game, 'home')
-    away_team = team_name(game, 'away')
-    odds = get_odds(game, odds_response)
-    home_odds = str(odds.get('home'))
-    away_odds = str(odds.get('away'))
-    return Game(
-        id=game['gameId'],
-        sport='NBA',
-        homeTeam=home_team,
-        awayTeam=away_team,
-        date=date,
-        time=parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone(tz=None).strftime("%H:%M"),
-        homeOdds=home_odds,
-        awayOdds=away_odds
-    )
-
-def get_todays_games():
-    board = scoreboard.ScoreBoard()
-    date = board.score_board_date
-    games = board.games.get_dict()
-    odds_response = call_odds_api()
-    games_list = [create_game_object(game, date, odds_response) for game in games]
-    return GamesResponse(list=games_list)
+def get_games_by_date(date):
+    if is_data_expired():
+        odds_response = call_odds_api()
+        games = parse_response_and_store_games(odds_response)
+        games_for_date = [game for game in games if game.time == date.strftime("%Y-%m-%d")]
+        return GamesResponse(list=games_for_date)
+    else:
+        session = connect_to_db()
+        games = session.query(Odds).filter(cast(Odds.time, Date) == date).all()
+        games_list = [
+            Game(
+                id=game.id,
+                sport='NBA',
+                homeTeam=game.home_team,
+                awayTeam=game.away_team,
+                time=game.time.strftime("%Y-%m-%d %H:%M"),
+                homeOdds=game.home_odds,
+                awayOdds=game.away_odds
+            )
+            for game in games
+        ]
+        return GamesResponse(list=games_list)

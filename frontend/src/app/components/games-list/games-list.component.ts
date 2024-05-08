82f9ac92c9aee0120ecf2396880ad285b48498ff
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-05-08 17:02:18 -0600

    Fix bug in displaying alert messages
    Display appropriate alert message when either connection to database is lost or no games are found for a given day.

--- frontend/src/app/components/games-list/games-list.component.ts
+++ frontend/src/app/components/games-list/games-list.component.ts
@@ -4,31 +4,39 @@ import { AlertMessageComponent } from '../alert-message/alert-message.component'
 import { GameComponent } from '../game/game.component';
 import { Store } from '@ngrx/store';
 import { nbaFeature } from '../nba/state';
-import { NbaCommands } from '../nba/state/actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-games-list',
   standalone: true,
   template: `
-    @if(loaded()) { @if(list().length === 0) {
-      <app-alert-message message="No games found for today." />
-    } @else { 
-        @for(item of list(); track item.id) {
-          <app-game [game]="item" />
-        } 
-      } 
-    } @else {
-      <div class="loading-container">
-        <span class="loading loading-bars loading-lg"></span>
-      </div>
-    }`,
+    <ng-container *ngIf="loaded() && !error()">
+      <ng-container *ngIf="list().length === 0">
+        <app-alert-message message="No games found for today." />
+      </ng-container>
+      <ng-container *ngIf="list().length > 0">
+        <app-game
+          *ngFor="let item of list(); trackBy: trackById"
+          [game]="item"
+        ></app-game>
+      </ng-container>
+    </ng-container>
+    <div *ngIf="!loaded() && !error()" class="loading-container">
+      <span class="loading loading-bars loading-lg"></span>
+    </div>
+  `,
   styles: [
     '.loading-container { display: flex; justify-content: center; margin-top: 48px }',
   ],
-  imports: [AlertMessageComponent, GameComponent],
+  imports: [AlertMessageComponent, GameComponent, CommonModule],
 })
 export class GamesListComponent {
   private store = inject(Store);
   list = input.required<Game[]>();
   loaded = this.store.selectSignal(nbaFeature.selectIsLoaded);
+  error = this.store.selectSignal(nbaFeature.selectError);
+
+  trackById(index: number, item: Game) {
+    return item.id;
+  }
 }
--- frontend/src/app/components/nba/nba.component.ts
+++ frontend/src/app/components/nba/nba.component.ts
@@ -4,37 +4,53 @@ import { GamesListComponent } from '../games-list/games-list.component';
 import { Store } from '@ngrx/store';
 import { nbaFeature } from './state';
 import { NbaCommands } from './state/actions';
-import { AlertMessageComponent } from "../alert-message/alert-message.component";
+import { AlertMessageComponent } from '../alert-message/alert-message.component';
 
 @Component({
-    selector: 'app-nba',
-    standalone: true,
-    template: ` <div class="join flex justify-center my-5">
-                <button class="join-item btn" (click)="previousDay()" [disabled]="isCurrentDate()">«</button>
-                <button class="join-item btn">{{ selectedDate | date: 'EEEE, MMMM d' }}</button>
-                <button class="join-item btn" (click)="nextDay()">»</button>
-              </div>
-              <ng-container *ngIf="games() && games().length > 0; else noGames">
-                <app-games-list [list]="games()"></app-games-list>
-              </ng-container>
-              <ng-template #noGames>
-                <app-alert-message message="Error loading games. Please check connection" />
-              </ng-template>`,
-    styles: [],
-    imports: [DatePipe, CommonModule, GamesListComponent, AlertMessageComponent]
+  selector: 'app-nba',
+  standalone: true,
+  template: `
+    <div class="join flex justify-center my-5">
+      <button
+        class="join-item btn"
+        (click)="previousDay()"
+        [disabled]="isCurrentDate()"
+      >
+        «
+      </button>
+      <button class="join-item btn">
+        {{ selectedDate | date : 'EEEE, MMMM d' }}
+      </button>
+      <button class="join-item btn" (click)="nextDay()">»</button>
+    </div>
+    <ng-container *ngIf="games()">
+      <app-games-list [list]="games()"></app-games-list>
+    </ng-container>
+    <app-alert-message
+      *ngIf="error()"
+      message="Error loading games."
+    />
+  `,
+  styles: [],
+  imports: [DatePipe, CommonModule, GamesListComponent, AlertMessageComponent],
 })
 export class NbaComponent {
   selectedDate: Date = new Date();
   constructor(private store: Store) {}
   games = this.store.selectSignal(nbaFeature.selectNbaGames);
+  error = this.store.selectSignal(nbaFeature.selectError);
 
   previousDay() {
-    this.selectedDate = new Date(this.selectedDate.getTime() - 24 * 60 * 60 * 1000);
+    this.selectedDate = new Date(
+      this.selectedDate.getTime() - 24 * 60 * 60 * 1000
+    );
     this.store.dispatch(NbaCommands.loadGames({ date: this.selectedDate }));
   }
 
   nextDay() {
-    this.selectedDate = new Date(this.selectedDate.getTime() + 24 * 60 * 60 * 1000);
+    this.selectedDate = new Date(
+      this.selectedDate.getTime() + 24 * 60 * 60 * 1000
+    );
     this.store.dispatch(NbaCommands.loadGames({ date: this.selectedDate }));
   }
 

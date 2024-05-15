ce1a0f095790f1ee92f8adc88e178f843bad6ddf
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-05-14 18:17:54 -0600

    Add form validation
    Display error messages that indicate that username and password are required when relevant. Disable form submission when form is invalid.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,30 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <div class="modal-box">
       <h3 class="font-bold text-lg mb-2">Login</h3>
-      <form>
-        <input
+      <form [formGroup]="loginForm" (ngSubmit)="onSubmit()" >
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Username</span>
+          </label>
+          <input
           type="text"
+          formControlName="username"
           placeholder="Username"
           class="input input-bordered w-full"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          class="input input-bordered w-full mt-2"
-        />
-        <div class="modal-action">
-          <button type="submit" class="btn btn-primary">Login</button>
-          <label for="login-modal" class="btn">Cancel</label>
+          />
+          <div *ngIf="loginForm.get('username')?.invalid && (loginForm.get('username')?.dirty || loginForm.get('username')?.touched)" class="text-error mt-1" >
+            <div *ngIf="loginForm.get('username')?.errors?.['required']">Username is required.</div>
+          </div>
+          <div class="form-control mt-2">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+            type="password"
+            formControlName="password"
+            placeholder="Password"
+            class="input input-bordered w-full mt-2"
+            />
+            <div *ngIf="loginForm.get('password')?.invalid && (loginForm.get('password')?.dirty || loginForm.get('password')?.touched)" class="text-error mt-1" >
+              <div *ngIf="loginForm.get('password')?.errors?.['required']">Password is required.</div>
+            </div>
+          </div>
+          <div class="modal-action">
+            <button type="submit" class="btn btn-primary" [disabled]="loginForm.invalid">Login</button>
+            <label for="login-modal" class="btn">Cancel</label>
+          </div>
         </div>
       </form>
     </div>
   `,
   styles: ``,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted');
+    }
+  }
+}

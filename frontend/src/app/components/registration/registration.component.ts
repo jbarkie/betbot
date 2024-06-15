8ba3e758baf8eb733fa9cc61705c720d7035e23d
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-06-14 18:14:51 -0600

    Add component
    Skeleton component for user registration.

---
+++ frontend/src/app/components/registration/registration.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      registration works!
+    </p>
+  `,
+  styles: ``
+})
+export class RegistrationComponent {
+
+}

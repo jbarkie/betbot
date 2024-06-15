99cca6436d50a0e8e948475ea4ef785592f1a2ef
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-06-14 18:15:07 -0600

    Add route
    For registration component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { NbaComponent } from './components/nba/nba.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 export const routes: Routes = [
   {
     path: 'nba',
     component: NbaComponent,
   },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  }
 ];

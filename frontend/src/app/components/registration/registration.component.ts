8f381e5d83afe2151cb168e66c3713f1bdcb0b74
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-06-19 20:42:27 -0600

    Add initial registration form

--- frontend/src/app/components/registration/registration.component.ts
+++ frontend/src/app/components/registration/registration.component.ts
@@ -5,9 +5,48 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      registration works!
-    </p>
+    <h3 class="font-bold text-lg mb-2 text-center">Register for an account</h3>
+    <form class="mx-auto w-96">
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">Username</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Username"
+          class="input input-bordered w-full"
+        />
+      </div>
+      <div class="form-control mt-2">
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Password"
+          class="input input-bordered w-full mt-2"
+        />
+      </div>
+      <div class="form-control mt-2">
+        <label class="label">
+          <span class="label-text">Confirm Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          class="input input-bordered w-full mt-2"
+        />
+      </div>
+      <div class="form-control mt-2">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input
+          type="email"
+          placeholder="Email"
+          class="input input-bordered w-full mt-2"
+        />
+      </div>
   `,
   styles: ``
 })

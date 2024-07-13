f4c165e96c2899e57d5ef32275abe8c6bf274d88
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-07-12 19:12:26 -0600

    Update function definition
    Ensure that an Authenticated User instance is only created if the given username is actually found in the database. Also update import statement to use appropriate Users table model.

--- api/src/login.py
+++ api/src/login.py
@@ -1,16 +1,18 @@
-from api.src.models.auth import User, AuthenticatedUser
+from api.src.models.auth import AuthenticatedUser
+from api.src.models.tables import Users
 from api.src.utils import connect_to_db
 
 
-def get_user_by_username(username: str) -> User:
+def get_user_by_username(username: str) -> AuthenticatedUser:
     session = connect_to_db()
-    user = session.query(User).filter_by(username=username).first()
-    user = AuthenticatedUser(
-        username=user.username,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        email=user.email,
-        hashed_password=user.password
-    )
+    user = session.query(Users).filter_by(username=username).first()
+    if user:
+        user = AuthenticatedUser(
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            hashed_password=user.password
+        )
     return user
 

2436a1acd323feeb00818be290bf7c9029b0a11c
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-07-18 19:31:10 -0600

    Hash password
    Before storing in database.

--- api/src/register.py
+++ api/src/register.py
@@ -1,10 +1,11 @@
 from api.src.models.tables import Users
 from api.src.utils import connect_to_db
-
+import bcrypt
 
 def register_user(username: str, first_name: str, last_name: str, email: str, password: str):
     session = connect_to_db()
-    user = Users(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    user = Users(username=username, first_name=first_name, last_name=last_name, email=email, password=hashed)
     session.add(user)
     session.commit()
     session.close()

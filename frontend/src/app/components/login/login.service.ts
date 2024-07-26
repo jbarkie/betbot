60b86d19f92d37990136d3665b624bba35fceb3b
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-07-25 20:33:45 -0600

    Change request format
    To form data instead of JSON object.

--- frontend/src/app/components/login/login.service.ts
+++ frontend/src/app/components/login/login.service.ts
@@ -1,25 +1,32 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, catchError, of } from "rxjs";
-import { LoginRequest, LoginResponse } from "../models";
-import { environment } from "../../../environments/environment";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { LoginRequest, LoginResponse } from '../models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-    private loginUrl = environment.apiUrl + '/login';
+  private loginUrl = environment.apiUrl + '/login';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    login(request: LoginRequest): Observable<LoginResponse> {
-        return this.http.post<LoginResponse>(this.loginUrl, request)
-            .pipe(
-                catchError(this.handleError<LoginResponse>('login'))
-            );
-    }
+  login(request: LoginRequest): Observable<LoginResponse> {
+    const body = new HttpParams()
+      .set('username', request.username)
+      .set('password', request.password);
 
-    private handleError<T>(operation = 'operation', result?: T) {
+    return this.http
+      .post<LoginResponse>(this.loginUrl, body.toString(), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .pipe(catchError(this.handleError<LoginResponse>('login')));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       console.log(`${operation} failed: ${error.message}`);

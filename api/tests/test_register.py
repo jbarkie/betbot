b691642dd9b82d882d74f6d733875af7693da926
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-07-29 12:49:26 -0600

    Add unit test
    To cover case where user registers and username already exists in database.

--- api/tests/test_register.py
+++ api/tests/test_register.py
@@ -2,7 +2,6 @@ from fastapi.testclient import TestClient
 from api.src.main import app
 from unittest.mock import patch, MagicMock
 from api.src.register import register_user
-import bcrypt
 
 client = TestClient(app)
 
@@ -70,3 +69,25 @@ def test_register_endpoint_integration():
         mock_session.add.assert_called_once_with(mock_user)
         mock_session.commit.assert_called_once()
         mock_session.close.assert_called_once()
+
+def test_register_endpoint_integration_user_exists():
+    mock_session = MagicMock()
+    mock_user = MagicMock()
+    mock_users_constructor = MagicMock(return_value=mock_user)
+
+    with patch('api.src.register.connect_to_db', return_value=mock_session), \
+         patch('api.src.register.Users', mock_users_constructor), \
+         patch('api.src.main.get_user_by_username', return_value=mock_user), \
+         patch('bcrypt.hashpw', return_value=b'hashed_password'):
+        
+        register_data = {
+            "username": "existinguser",
+            "first_name": "Existing",
+            "last_name": "User",
+            "email": "newuser@example.com",
+            "password": "password123"
+        }
+        response = client.post("/register", json=register_data)
+
+        assert response.status_code == 403
+        assert response.json() == {"detail": "User already exists"}

2003e64e1368f75936b7ecd7bc038b3492aec6cc
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-03 10:21:49 -0600

    Add service
    For handling the display of the notification.

---
+++ frontend/src/app/components/toast/toast.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface Toast {
+  message: string;
+  type: 'success' | 'error';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  private toastSubject = new Subject<Toast>();
+  toast$ = this.toastSubject.asObservable();
+
+  showSuccess(message: string) {
+    this.toastSubject.next({ message, type: 'success' });
+  }
+
+  showError(message: string) {
+    this.toastSubject.next({ message, type: 'error' });
+  }
+}

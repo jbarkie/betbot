2221f795c662602d32d81e9e476b42a449963bfe
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-04 12:27:26 -0600

    Add register actions
    For storing authentication info in the application state.

--- frontend/src/app/state/auth/auth.actions.ts
+++ frontend/src/app/state/auth/auth.actions.ts
@@ -3,6 +3,8 @@ import { createActionGroup, emptyProps, props } from '@ngrx/store';
 export const authActions = createActionGroup({
     source: 'Auth',
     events: {
+        'Register Success': props<{ token: string }>(),
+        'Register Failure': props<{ error: string }>(),
         'Login Success': props<{ token: string }>(),
         'Login Failure': props<{ error: string }>(),
         'Logout': emptyProps(),

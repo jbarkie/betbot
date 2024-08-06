cf274374f94fa44d33cef3bb086bc5c122380685
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-05 20:16:41 -0600

    Refactor component
    Use new wrapper component.

--- frontend/src/app/components/nba/nba.component.ts
+++ frontend/src/app/components/nba/nba.component.ts
@@ -1,65 +1,25 @@
-import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { GamesListComponent } from '../games-list/games-list.component';
-import { Store } from '@ngrx/store';
 import { nbaFeature } from './state';
 import { NbaCommands } from './state/actions';
-import { AlertMessageComponent } from '../alert-message/alert-message.component';
+import { SportWrapperComponent } from "../sport-wrapper/sport-wrapper.component";
+import { Game } from '../models';
 
 @Component({
   selector: 'app-nba',
   standalone: true,
   template: `
-    <div class="join flex justify-center my-5">
-      <button
-        class="join-item btn"
-        (click)="previousDay()"
-        [disabled]="isCurrentDate()"
-      >
-        «
-      </button>
-      <button class="join-item btn">
-        {{ selectedDate | date : 'EEEE, MMMM d' }}
-      </button>
-      <button class="join-item btn" (click)="nextDay()">»</button>
-    </div>
-    <ng-container *ngIf="games()">
-      <app-games-list [list]="games()"></app-games-list>
-    </ng-container>
-    <app-alert-message
-      *ngIf="error()"
-      message="Error loading games."
-    />
+    <app-sport-wrapper
+      sportName="NBA"
+      [gamesSelector]="selectNbaGames"
+      [errorSelector]="selectError"
+      [loadGamesAction]="NbaCommands.loadGames"
+    ></app-sport-wrapper>
   `,
   styles: [],
-  imports: [DatePipe, CommonModule, GamesListComponent, AlertMessageComponent],
+  imports: [SportWrapperComponent],
 })
 export class NbaComponent {
-  selectedDate: Date = new Date();
-  constructor(private store: Store) {}
-  games = this.store.selectSignal(nbaFeature.selectNbaGames);
-  error = this.store.selectSignal(nbaFeature.selectError);
-
-  previousDay() {
-    this.selectedDate = new Date(
-      this.selectedDate.getTime() - 24 * 60 * 60 * 1000
-    );
-    this.store.dispatch(NbaCommands.loadGames({ date: this.selectedDate }));
-  }
-
-  nextDay() {
-    this.selectedDate = new Date(
-      this.selectedDate.getTime() + 24 * 60 * 60 * 1000
-    );
-    this.store.dispatch(NbaCommands.loadGames({ date: this.selectedDate }));
-  }
-
-  isCurrentDate() {
-    const today = new Date();
-    return (
-      this.selectedDate.getDate() === today.getDate() &&
-      this.selectedDate.getMonth() === today.getMonth() &&
-      this.selectedDate.getFullYear() === today.getFullYear()
-    );
-  }
+  selectNbaGames = (state: Game[]) => nbaFeature.selectNbaGames(state);
+  selectError = (state: any) => nbaFeature.selectError(state);
+  NbaCommands = NbaCommands;
 }

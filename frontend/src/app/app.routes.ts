8a328d343919bb2aa58989f31fa7123639255f1f
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-06 19:18:56 -0600

    Add route
    For MLB component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NbaComponent } from './components/nba/nba.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { MlbComponent } from './components/mlb/mlb.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegistrationComponent,
-  }
+  },
+  {
+    path: 'mlb',
+    component: MlbComponent,
+  },
 ];

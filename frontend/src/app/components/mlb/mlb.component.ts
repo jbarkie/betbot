cdb9159c196e25c42fb3c2996a55f0b26f7147fd
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-06 19:18:41 -0600

    Add initial component

---
+++ frontend/src/app/components/mlb/mlb.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mlb',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      mlb works!
+    </p>
+  `,
+  styles: ``
+})
+export class MlbComponent {
+
+}

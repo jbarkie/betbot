2d6779ab19f8851c124f18d52dd0238c29bd4357
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-08 20:27:14 -0600

    Remove unused import

--- api/src/main.py
+++ api/src/main.py
@@ -7,7 +7,7 @@ from api.src.models.auth import AuthenticatedUser, LoginResponse, RegisterReques
 from api.src.games import get_games_by_date
 from api.src.models.nba import GamesResponse
 import uvicorn
-from typing import Any, Annotated
+from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 import traceback
 from api.src.register import register_user

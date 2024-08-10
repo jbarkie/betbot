0d547d367218f4aaddc6b1bffd7800e8a4c321be
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-09 20:30:37 -0600

    Initialize sport wrapper
    With relevant MLB state information.

--- frontend/src/app/components/mlb/mlb.component.ts
+++ frontend/src/app/components/mlb/mlb.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
+import { SportWrapperComponent } from '../sport-wrapper/sport-wrapper.component';
+import { mlbFeature, MlbState } from './state';
+import { MLBCommands } from './state/actions';
 
 @Component({
   selector: 'app-mlb',
   standalone: true,
-  imports: [],
+  imports: [SportWrapperComponent],
   template: `
-    <p>
-      mlb works!
-    </p>
+    <app-sport-wrapper
+      sportName="MLB"
+      [gamesSelector]="selectMlbGames"
+      [errorSelector]="selectError"
+      [loadGamesAction]="loadGames"
+    ></app-sport-wrapper>
   `,
   styles: ``
 })
 export class MlbComponent {
-
+  selectMlbGames = (state: { mlb: MlbState }) => mlbFeature.selectMlbGames(state);
+  selectError = (state: { mlb: MlbState }) => mlbFeature.selectError(state);
+  loadGames = MLBCommands.loadGames;
 }

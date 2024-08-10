a2e2b9939b3f842cb1f1bf7e011d11e18c8d5e49
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-09 20:27:26 -0600

    Add effects
    For fetching and storing MLB games and odds data.

---
+++ frontend/src/app/components/mlb/state/mlb-effects.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { environment } from '../../../../environments/environment';
+import { appActions } from '../../../state/actions';
+import { MLBCommands, MLBDocuments } from './actions';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { Game } from '../../models';
+
+@Injectable()
+export class MlbEffects {
+  constructor(private actions$: Actions, private httpClient: HttpClient) {}
+
+  readonly baseUrl = environment.apiUrl;
+  onStartup = createEffect(() =>
+    this.actions$.pipe(
+      ofType(appActions.applicationStarted),
+      map(() => MLBCommands.loadGames({ date: new Date() }))
+    )
+  );
+  loadGames$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MLBCommands.loadGames),
+      switchMap((action) => {
+        const datePipe = new DatePipe('en-US');
+        const formattedDate = datePipe.transform(action.date, 'yyyy-MM-dd');
+        const params = formattedDate
+          ? new HttpParams().set('date', formattedDate)
+          : new HttpParams();
+        return this.httpClient
+          .get<{ list: Game[] }>(this.baseUrl + '/mlb/games', {
+            params,
+            responseType: 'json',
+          })
+          .pipe(
+            map((response) => response.list),
+            map((payload) => MLBDocuments.games({ payload })),
+            catchError((error) => {
+              return of(MLBCommands.loadGamesError({ error }));
+            })
+          );
+      })
+    )
+  );
+}

8bd4f54e6506b2bf84612aa11f7b07d86a146f13
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-11 16:26:39 -0600

    Update config
    Add provider for AuthInterceptor.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,25 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore, provideState } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { reducers } from './state';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { nbaFeature } from './components/nba/state';
 import { provideEffects } from '@ngrx/effects';
 import { NbaEffects } from './components/nba/state/nba-effects';
 import { mlbFeature } from './components/mlb/state';
 import { MlbEffects } from './components/mlb/state/mlb-effects';
+import { authInterceptProvider } from './services/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(reducers),
-    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
     provideState(nbaFeature),
     provideEffects([NbaEffects]),
     provideState(mlbFeature),
     provideEffects([MlbEffects]),
     provideStoreDevtools(),
+    authInterceptProvider,
   ],
 };

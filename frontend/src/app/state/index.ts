0f06de008093424ddf61ab60c04057904d3d7337
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-11 16:21:27 -0600

    Update app state
    To include sub-state of each sport.

--- frontend/src/app/state/index.ts
+++ frontend/src/app/state/index.ts
@@ -1,10 +1,16 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { authReducer, AuthState } from './auth/auth.reducer';
+import { mlbFeature, MlbState } from '../components/mlb/state';
+import { nbaFeature, NbaState } from '../components/nba/state';
 
 export interface ApplicationState {
-    auth: AuthState;
+  auth: AuthState;
+  mlb: MlbState;
+  nba: NbaState;
 }
 
 export const reducers: ActionReducerMap<ApplicationState> = {
-    auth: authReducer,
+  auth: authReducer,
+  mlb: mlbFeature.reducer,
+  nba: nbaFeature.reducer,
 };

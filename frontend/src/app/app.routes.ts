41d1c2e241e39664daa9d226e7802c245acc754c
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-11 16:32:56 -0600

    Secure routes
    Such that users must login or register before accessing.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { NbaComponent } from './components/nba/nba.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { MlbComponent } from './components/mlb/mlb.component';
+import { authGuard } from './services/auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'nba',
     component: NbaComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'register',
@@ -15,5 +17,6 @@ export const routes: Routes = [
   {
     path: 'mlb',
     component: MlbComponent,
+    canActivate: [authGuard],
   },
 ];

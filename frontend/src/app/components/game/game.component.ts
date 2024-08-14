cb6e43086087c4587c93e014dfca3d6cf2428e09
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-14 15:56:24 -0600

    Update ordering on page
    To match typical convention of where home and away teams appear.

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -9,7 +9,7 @@ import { Game } from '../models';
   template: ` 
   <div class="card card-side bg-base-100 shadow-xl mx-auto my-5 max-w-2xl">
   <figure>
-    <img [src]="getImageSrc(game().sport, game().homeTeam)" [alt]="game().homeTeam + ' logo'" class="w-28 mx-5" />
+    <img [src]="getImageSrc(game().sport, game().awayTeam)" [alt]="game().awayTeam + ' logo'" class="w-28 mx-5" />
   </figure>
   <div class="card-body">
     <h2 class="card-title">{{ game().awayTeam }} &#64; <br/> {{ game().homeTeam }}</h2>
@@ -17,15 +17,15 @@ import { Game } from '../models';
       <p class="text-md">No odds available</p>
     }
     @else {
-      <p>{{ game().homeTeam }} ML: {{ game().homeOdds }}</p>
       <p>{{ game().awayTeam }} ML: {{ game().awayOdds }}</p>
+      <p>{{ game().homeTeam }} ML: {{ game().homeOdds }}</p>
     }
     <div class="card-actions justify-end">
       <button class="btn btn-primary">Analyze</button>
     </div>
   </div>
   <figure>
-    <img [src]="getImageSrc(game().sport, game().awayTeam)" [alt]="game().awayTeam + ' logo'" class="w-28 mx-5" />
+    <img [src]="getImageSrc(game().sport, game().homeTeam)" [alt]="game().homeTeam + ' logo'" class="w-28 mx-5" />
   </figure>
 </div>`,
   styles: [],

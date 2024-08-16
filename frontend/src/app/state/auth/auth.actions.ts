13070dfc49cd180857d54d6b0f1fd603f01cc4b5
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-16 09:16:18 -0600

    Add new actions
    To check whether user is already authenticated on app start.

--- frontend/src/app/state/auth/auth.actions.ts
+++ frontend/src/app/state/auth/auth.actions.ts
@@ -3,6 +3,9 @@ import { createActionGroup, emptyProps, props } from '@ngrx/store';
 export const authActions = createActionGroup({
     source: 'Auth',
     events: {
+        'Initialize Auth': emptyProps(),
+        'Initialize Auth Success': props<{ token: string }>(),
+        'Initialize Auth Failure': emptyProps(),
         'Register Success': props<{ token: string }>(),
         'Register Failure': props<{ error: string }>(),
         'Login Success': props<{ token: string }>(),

f9da4b73bc63dc31fce876149db4823b4cae0cfd
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-17 13:58:50 -0600

    Update page header unit tests
    Given updates to component.

--- frontend/src/app/components/page-header/page-header.component.spec.ts
+++ frontend/src/app/components/page-header/page-header.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 import { PageHeaderComponent } from './page-header.component';
 
@@ -8,10 +13,13 @@ describe('PageHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageHeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [PageHeaderComponent, HttpClientTestingModule],
+      providers: [
+        provideMockStore({}),
+        { provide: ActivatedRoute, useValue: { params: of({}) } },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PageHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +27,15 @@ describe('PageHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a header element', () => {
+    const header = fixture.debugElement.query(By.css('header'));
+    expect(header).toBeTruthy();
+  });
+
+  it('should include the NavBarComponent', () => {
+    const navBar = fixture.debugElement.query(By.css('app-nav-bar'));
+    expect(navBar).toBeTruthy();
   });
 });

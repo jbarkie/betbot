25e94deab7d8a6637eca5f456a62847e74a5b421
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-20 11:33:07 -0600

    Add route
    For NHL component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,20 +3,26 @@ import { NbaComponent } from './components/nba/nba.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { MlbComponent } from './components/mlb/mlb.component';
 import { authGuard } from './services/auth/auth.guard';
+import { NhlComponent } from './components/nhl/nhl.component';
 
 export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  },
   {
     path: 'nba',
     component: NbaComponent,
     canActivate: [authGuard],
   },
   {
-    path: 'register',
-    component: RegistrationComponent,
-  },
-  {
     path: 'mlb',
     component: MlbComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'nhl',
+    component: NhlComponent,
     canActivate: [authGuard],
   },
 ];

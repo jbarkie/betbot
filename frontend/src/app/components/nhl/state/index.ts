66905f10f18fef91c3cfd52775275b67ac378245
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-21 14:09:23 -0600

    Update feature
    Add selectors for loading games and handling errors.

--- frontend/src/app/components/nhl/state/index.ts
+++ frontend/src/app/components/nhl/state/index.ts
@@ -1,8 +1,8 @@
 import { createEntityAdapter } from '@ngrx/entity';
-import { createFeature, createReducer, on } from '@ngrx/store';
+import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
 import { SportsState } from '../../sport-wrapper/state';
 import { Game } from '../../models';
-import { NHLDocuments } from './actions';
+import { NHLCommands, NHLDocuments } from './actions';
 
 export interface NhlState extends SportsState {}
 
@@ -18,6 +18,33 @@ export const nhlFeature = createFeature({
     initialState,
     on(NHLDocuments.games, (s, a) =>
       adapter.setAll(a.payload, { ...s, isLoaded: true, error: null })
-    )
+    ),
+    on(NHLCommands.loadGamesError, (s, a) => ({
+      ...s,
+      isLoaded: true,
+      error: a.error,
+    }))
   ),
+  extraSelectors: ({ selectNhlState }) => {
+    const { selectAll } = adapter.getSelectors();
+    const nhlArray = createSelector(selectNhlState, (s) => selectAll(s));
+    return {
+      selectNhlGames: createSelector(nhlArray, (games) =>
+        games.map(
+          (game) =>
+            ({
+              id: game.id,
+              sport: game.sport,
+              homeTeam: game.homeTeam,
+              awayTeam: game.awayTeam,
+              date: game.date,
+              time: game.time,
+              homeOdds: game.homeOdds,
+              awayOdds: game.awayOdds,
+            } as Game)
+        )
+      ),
+      selectError: createSelector(selectNhlState, (s) => s.error ?? ''),
+    };
+  },
 });

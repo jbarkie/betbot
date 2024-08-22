64d4b8ab7795266bc8aba3e1e226c3334df9ea27
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-22 14:54:03 -0600

    Update unit test
    Given changes made to NHL component.

--- frontend/src/app/components/nhl/nhl.component.spec.ts
+++ frontend/src/app/components/nhl/nhl.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { NhlComponent } from './nhl.component';
 
 describe('NhlComponent', () => {
   let component: NhlComponent;
   let fixture: ComponentFixture<NhlComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NhlComponent]
-    })
-    .compileComponents();
-    
+      imports: [NhlComponent],
+      providers: [
+        provideMockStore({
+          initialState: {
+            nhl: {
+              entities: {},
+              ids: [],
+              isLoaded: false,
+              error: '',
+            },
+          },
+        }),
+      ],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(NhlComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- frontend/src/app/components/nhl/nhl.component.ts
+++ frontend/src/app/components/nhl/nhl.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { ApplicationState } from '../../state';
+import { nhlFeature } from './state';
+import { SportWrapperComponent } from '../sport-wrapper/sport-wrapper.component';
+import { NHLCommands } from './state/actions';
 
 @Component({
   selector: 'app-nhl',
   standalone: true,
-  imports: [],
+  imports: [SportWrapperComponent],
   template: `
-    <p>
-      nhl works!
-    </p>
+    <app-sport-wrapper
+      sportName="NHL"
+      [gamesSelector]="selectNhlGames"
+      [errorSelector]="selectError"
+      [loadedSelector]="selectLoaded"
+      [loadGamesAction]="loadGames"
+    ></app-sport-wrapper>
   `,
-  styles: ``
+  styles: ``,
 })
 export class NhlComponent {
-
+  selectNhlGames = (state: ApplicationState) =>
+    nhlFeature.selectNhlGames(state);
+  selectError = (state: ApplicationState) => nhlFeature.selectError(state);
+  selectLoaded = (state: ApplicationState) => nhlFeature.selectIsLoaded(state);
+  loadGames = NHLCommands.loadGames;
 }

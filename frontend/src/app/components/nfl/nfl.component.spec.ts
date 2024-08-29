4c4b51adf5c6693a872537260a06c48817238ac3
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-08-29 14:15:46 -0600

    Implement component
    Use SportsWrapper component to display NFL games for a given date.

--- frontend/src/app/components/nfl/nfl.component.spec.ts
+++ frontend/src/app/components/nfl/nfl.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { NflComponent } from './nfl.component';
 
 describe('NflComponent', () => {
   let component: NflComponent;
   let fixture: ComponentFixture<NflComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NflComponent]
-    })
-    .compileComponents();
-    
+      imports: [NflComponent],
+      providers: [
+        provideMockStore({
+          initialState: {
+            nfl: {
+              entities: {},
+              ids: [],
+              isLoaded: false,
+              error: '',
+            },
+          },
+        }),
+      ],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(NflComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- frontend/src/app/components/nfl/nfl.component.ts
+++ frontend/src/app/components/nfl/nfl.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { SportWrapperComponent } from '../sport-wrapper/sport-wrapper.component';
+import { ApplicationState } from '../../state';
+import { nflFeature } from './state';
+import { NFLCommands } from './state/actions';
 
 @Component({
   selector: 'app-nfl',
   standalone: true,
-  imports: [],
+  imports: [SportWrapperComponent],
   template: `
-    <p>
-      nfl works!
-    </p>
+    <app-sport-wrapper
+      sportName="NFL"
+      [gamesSelector]="selectNflGames"
+      [errorSelector]="selectError"
+      [loadedSelector]="selectLoaded"
+      [loadGamesAction]="loadGames"
+    ></app-sport-wrapper>
   `,
-  styles: ``
+  styles: ``,
 })
 export class NflComponent {
-
+  selectNflGames = (state: ApplicationState) =>
+    nflFeature.selectNflGames(state);
+  selectError = (state: ApplicationState) => nflFeature.selectError(state);
+  selectLoaded = (state: ApplicationState) => nflFeature.selectIsLoaded(state);
+  loadGames = NFLCommands.loadGames;
 }

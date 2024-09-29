9ecbf4e1d54e125659c9a5241b4d13f26803734a
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-09-29 15:31:40 -0600

    Scaffold current season data fetching
    Add initial implementation of logic that fetches data for current season.

--- machine-learning/data/collection/mlb.py
+++ machine-learning/data/collection/mlb.py
@@ -1,5 +1,6 @@
 import statsapi
 import logging
+import pprint
 from datetime import datetime
 from colorlog import ColoredFormatter
 
@@ -27,9 +28,30 @@ def configure_logging():
     ch.setFormatter(formatter)
     rootLogger.addHandler(ch)
 
+def fetch_current_season():
+    season_data = statsapi.latest_season()
+    season_start = season_data['regularSeasonStartDate']
+    is_season_started = season_start < datetime.today().strftime('%Y-%m-%d')
+
+    if is_season_started:
+        fetch_data_within_range(season_start, datetime.today().strftime('%Y-%m-%d'))
+    else:
+        logging.error('Current season has not started yet.')
+
+    return season_data
+
+def fetch_data_within_range(start_date, end_date):
+    try:
+        schedule = statsapi.schedule(start_date=start_date, end_date=end_date)
+        logging.debug('Games between %s and %s: %s', start_date, end_date, pprint.pformat(schedule))
+    except Exception as e:
+        logging.error('Error fetching data: %s', e)
+
 def main():
     configure_logging()
     logging.info('Starting')
+    season_data = fetch_current_season()
+    logging.info('Season data fetched for %s', season_data['seasonId'])
 
 if __name__ == '__main__':
     main()

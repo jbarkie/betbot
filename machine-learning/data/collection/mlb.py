63aaedacdac01772d9e7dccdbdfb976bbd471895
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-09-29 15:30:55 -0600

    Colorize log output

--- machine-learning/data/collection/mlb.py
+++ machine-learning/data/collection/mlb.py
@@ -1,13 +1,29 @@
 import statsapi
 import logging
+from datetime import datetime
+from colorlog import ColoredFormatter
 
 def configure_logging():
     logger = logging.getLogger('statsapi')
     logger.setLevel(logging.DEBUG)
     rootLogger = logging.getLogger()
     rootLogger.setLevel(logging.DEBUG)
+
     ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(levelname)8s - %(name)s - %(message)s')
+
+    formatter = ColoredFormatter(
+        "%(log_color)s%(asctime)s - %(levelname)-8s%(reset)s - %(light_blue)s%(name)s%(reset)s - %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            'DEBUG':    'cyan',
+            'INFO':     'green',
+            'WARNING':  'yellow',
+            'ERROR':    'red',
+            'CRITICAL': 'red,bg_white',
+        }
+    )
+
     ch.setFormatter(formatter)
     rootLogger.addHandler(ch)
 

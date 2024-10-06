17beb59dcd22dd0284180be4fb9adf467977c238
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-10-06 13:14:23 -0600

    Replace dependency
    Use different wrapper for accessing MLB stats API.

--- machine-learning/data/collection/mlb.py
+++ machine-learning/data/collection/mlb.py
@@ -1,6 +1,5 @@
-import statsapi
+import mlbstatsapi 
 import logging
-import pprint
 from datetime import datetime
 from colorlog import ColoredFormatter
 
@@ -28,30 +27,31 @@ def configure_logging():
     ch.setFormatter(formatter)
     rootLogger.addHandler(ch)
 
-def fetch_current_season():
-    season_data = statsapi.latest_season()
-    season_start = season_data['regularSeasonStartDate']
-    is_season_started = season_start < datetime.today().strftime('%Y-%m-%d')
+def fetch_current_season(mlb):
+    current_season_data = mlb.get_season(str(datetime.today().year))
+    regular_season_start_date = current_season_data.regularseasonstartdate
+
+    is_season_started = regular_season_start_date < datetime.today().strftime('%Y-%m-%d')
 
     if is_season_started:
-        fetch_data_within_range(season_start, datetime.today().strftime('%Y-%m-%d'))
+        fetch_data_within_range(regular_season_start_date, datetime.today().strftime('%Y-%m-%d'))
     else:
         logging.error('Current season has not started yet.')
 
-    return season_data
+    return current_season_data
 
 def fetch_data_within_range(start_date, end_date):
     try:
-        schedule = statsapi.schedule(start_date=start_date, end_date=end_date)
-        logging.debug('Games between %s and %s: %s', start_date, end_date, pprint.pformat(schedule))
+        logging.info('Fetching data from %s to %s', start_date, end_date)
     except Exception as e:
         logging.error('Error fetching data: %s', e)
 
 def main():
     configure_logging()
     logging.info('Starting')
-    season_data = fetch_current_season()
-    logging.info('Season data fetched for %s', season_data['seasonId'])
+    mlb = mlbstatsapi.Mlb()
+    season_data = fetch_current_season(mlb)
+    logging.debug('Current season: %s', season_data)
 
 if __name__ == '__main__':
     main()

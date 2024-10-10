6534164bdd92c4bd18b8383b2fcb39e2157aa6ec
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-10-10 16:23:59 -0600

    Add reference to new models
    That represent new tables to be added to database that will store historical MLB statistics.

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from api.src.models.tables import Base
+from machine_learning.data.models.mlb_models import Base
 
 from alembic import context
 

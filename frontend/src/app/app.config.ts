4acaca29dee07db0499f98b74ce6b7d0f33c6bee
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2024-12-27 16:43:18 -0700

    Update application config
    According to Angular 18 upgrade.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,33 +1,35 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore, provideState } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { reducers } from './state';
-import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
-import { nbaFeature } from './components/nba/state';
 import { provideEffects } from '@ngrx/effects';
-import { NbaEffects } from './components/nba/state/nba-effects';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { routes } from './app.routes';
 import { mlbFeature } from './components/mlb/state';
 import { MlbEffects } from './components/mlb/state/mlb-effects';
-import { authInterceptProvider } from './services/auth/auth.interceptor';
-import { AuthEffects } from './state/auth/auth.effects';
-import { NhlEffects } from './components/nhl/state/nhl-effects';
-import { nhlFeature } from './components/nhl/state';
+import { nbaFeature } from './components/nba/state';
+import { NbaEffects } from './components/nba/state/nba-effects';
 import { nflFeature } from './components/nfl/state';
 import { NflEffects } from './components/nfl/state/nfl-effects';
+import { nhlFeature } from './components/nhl/state';
+import { NhlEffects } from './components/nhl/state/nhl-effects';
+import { authInterceptor } from './services/auth/auth.interceptor';
+import { reducers } from './state';
+import { AuthEffects } from './state/auth/auth.effects';
+import { withHttpTransferCacheOptions } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(reducers),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideEffects([NbaEffects, MlbEffects, NhlEffects, NflEffects, AuthEffects]),
     provideState(nbaFeature),
     provideState(mlbFeature),
     provideState(nhlFeature),
     provideState(nflFeature),
-    provideStoreDevtools(),
-    authInterceptProvider
+    provideStoreDevtools()
   ],
 };

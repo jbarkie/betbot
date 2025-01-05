198be63b0ff428745da7ffa02489ec8e1bedf09d
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-01-04 17:47:52 -0700

    Update application config
    New state management processes with Angular 19 upgrade.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,19 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideEffects } from '@ngrx/effects';
-import { provideState, provideStore } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
-import { mlbFeature } from './components/mlb/state';
-import { MlbEffects } from './components/mlb/state/mlb-effects';
-import { NBAStore } from './components/nba/services/nba.store';
-import { nflFeature } from './components/nfl/state';
-import { NflEffects } from './components/nfl/state/nfl-effects';
-import { nhlFeature } from './components/nhl/state';
-import { NhlEffects } from './components/nhl/state/nhl-effects';
 import { authInterceptor } from './services/auth/auth.interceptor';
+import {
+  MLBStore,
+  NBAStore,
+  NFLStore,
+  NHLStore,
+} from './services/sports/sports.store';
 import { reducers } from './state';
+import { provideEffects } from '@ngrx/effects';
 import { AuthEffects } from './state/auth/auth.effects';
 
 export const appConfig: ApplicationConfig = {
@@ -21,11 +21,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideStore(reducers),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideEffects([MlbEffects, NhlEffects, NflEffects, AuthEffects]),
-    provideState(mlbFeature),
-    provideState(nhlFeature),
-    provideState(nflFeature),
+    provideEffects([AuthEffects]),
     provideStoreDevtools(),
+    provideHttpClientTesting(),
     NBAStore,
+    NHLStore,
+    NFLStore,
+    MLBStore,
   ],
 };

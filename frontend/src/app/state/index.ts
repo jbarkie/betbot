4f740f8f45b47a88076dd7c1596f9aaf8f1dba16
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-01-08 17:23:26 -0700

    Update application state
    Remove imports from and references to dead code.

--- frontend/src/app/state/index.ts
+++ frontend/src/app/state/index.ts
@@ -1,16 +1,10 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { authReducer, AuthState } from './auth/auth.reducer';
-import { mlbFeature, MlbState } from '../components/mlb/state';
-import { nbaFeature, NbaState } from '../components/nba/state';
 
 export interface ApplicationState {
   auth: AuthState;
-  mlb: MlbState;
-  nba: NbaState;
 }
 
 export const reducers: ActionReducerMap<ApplicationState> = {
   auth: authReducer,
-  mlb: mlbFeature.reducer,
-  nba: nbaFeature.reducer,
 };

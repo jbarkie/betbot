b533db036c0e1a4ef0900bd10412f4bdd76c0cdd
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-02-05 16:34:11 -0700

    Update interceptor
    To adhere with standards of Angular 19 update.

--- frontend/src/app/services/auth/auth.interceptor.ts
+++ frontend/src/app/services/auth/auth.interceptor.ts
@@ -5,23 +5,19 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { catchError, throwError } from 'rxjs';
 import { ToastService } from '../../components/toast/toast.service';
-import { ApplicationState } from '../../state';
-import { authActions } from '../../state/auth/auth.actions';
-import { AuthService } from './auth.service';
+import { AuthStore } from './auth.store';
 
 export const authInterceptor: HttpInterceptorFn = (
   request: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
-  const authService = inject(AuthService);
-  const store = inject(Store<ApplicationState>);
+  const authStore = inject(AuthStore);
   const toastService = inject(ToastService);
 
   console.log('intercepting request');
-  const token = authService.getToken();
+  const token = authStore.token();
   if (token) {
     request = request.clone({
       setHeaders: {
@@ -33,8 +29,8 @@ export const authInterceptor: HttpInterceptorFn = (
   return next(request).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
-        store.dispatch(authActions.logout());
-        store.dispatch(authActions.showLoginModal());
+        authStore.logout();
+        // TODO: show login modal?
         toastService.showError('Session expired. Please log in again.');
       }
       return throwError(() => error);

076f23678c6a28d210235773bdf08dfed1a429cb
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-02-05 16:30:58 -0700

    Remove stale references
    To dead code.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,27 +2,15 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth/auth.interceptor';
-import {
-  MLBStore,
-  NBAStore,
-  NFLStore,
-  NHLStore,
-} from './services/sports/sports.store';
-import { reducers } from './state';
-import { provideEffects } from '@ngrx/effects';
-import { AuthEffects } from './state/auth/auth.effects';
 import { AuthStore } from './services/auth/auth.store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideStore(reducers),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideEffects([AuthEffects]),
     provideStoreDevtools(),
     provideHttpClientTesting(),
     AuthStore,

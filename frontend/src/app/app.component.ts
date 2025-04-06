274183682845b3f537be1407c9373dfddc99b4d1
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-04-06 15:37:23 -0600

    Add component to landing page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,15 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { ToastComponent } from './components/toast/toast.component';
 import { AuthStore } from './services/auth/auth.store';
-
+import { HomeComponent } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PageHeaderComponent, ToastComponent],
+  imports: [RouterOutlet, PageHeaderComponent, ToastComponent, HomeComponent],
   template: `<div class="container mx-auto">
     <app-page-header />
     <main>
       <router-outlet />
+      <app-home />
     </main>
     <app-toast />
   </div>`,

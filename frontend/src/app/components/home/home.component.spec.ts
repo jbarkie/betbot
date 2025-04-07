e766ce1f15d66be8eb1dd2a8a643633b3b4d5cb1
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-04-07 08:55:06 -0600

    Update tests
    According to component updates.

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { AuthStore } from '../../services/auth/auth.store';
+import { By } from '@angular/platform-browser';
+
+const mockAuthStore = {
+  isAuthenticated: jest.fn().mockReturnValue(false),
+  showLoginModal: jest.fn(),
+};
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +15,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent],
+      providers: [{ provide: AuthStore, useValue: mockAuthStore }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -19,5 +26,33 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the proper verbiage and content', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Outsmart the Odds'
+    );
+    expect(compiled.querySelector('button').textContent).toContain(
+      'Login or Register Now!'
+    );
+  });
+
+  it('should call authStore.showLoginModal when login button is clicked', () => {
+    const loginButton = fixture.debugElement.query(By.css('.btn-primary'));
+
+    loginButton.nativeElement.click();
+
+    expect(mockAuthStore.showLoginModal).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call showRegistration method when button is clicked', () => {
+    const showRegistrationSpy = jest.spyOn(component, 'showRegistration');
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+
+    expect(showRegistrationSpy).toHaveBeenCalled();
+    expect(mockAuthStore.showLoginModal).toHaveBeenCalled();
   });
 });

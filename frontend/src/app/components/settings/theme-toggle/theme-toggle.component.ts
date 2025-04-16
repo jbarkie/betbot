059c42bf487ee7ed609d80062f18a4a9d03bd2e7
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-04-15 18:18:17 -0600

    Add initial component
    For toggling theme between light and dark mode.

---
+++ frontend/src/app/components/settings/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [],
+  template: `
+    <p>
+      theme-toggle works!
+    </p>
+  `,
+  styles: ``
+})
+export class ThemeToggleComponent {
+
+}

2d8ba7291be1c868f02fcdbcc7572ac80f950365
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-04-17 17:01:33 -0600

    Mock matchMedia for all tests
    To fix failing tests.

--- frontend/setup-jest.ts
+++ frontend/setup-jest.ts
@@ -1,3 +1,10 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
     
 setupZoneTestEnv();
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+    })),
+});

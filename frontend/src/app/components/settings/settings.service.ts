e6bf60528d03c52a87591ec1bbf1984baaee9068
Joe Barkie <26171310+jbarkie@users.noreply.github.com>
2025-04-24 15:50:07 -0600

    Add SettingsService
    Responsible for making actual POST HTTP request to server endpoint.

---
+++ frontend/src/app/components/settings/settings.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { SettingsRequest } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  private settingsUrl = environment.apiUrl + '/settings';
+
+  constructor(private httpClient: HttpClient) {}
+
+  updateSettings(request: SettingsRequest): Observable<void> {
+    return this.httpClient.post<void>(this.settingsUrl, request);
+  }
+}
